b73111d506b35ca70e799b3e195e4ece13ca4a59
kangqiwang <kangqiwang@outlook.com>
2018-02-13 22:00:21 +0000

    some other situation

--- python.py
+++ python.py
@@ -1,8 +1,9 @@
-s1="sujil is coding"
+s1="sujil is coding   "
+s1=s1.rstrip()
 a=list(s1)
 print(a)
 for i in range(len(a)-1,0,-1):
-	print("do something")
+	print("do something",a[i])
 	if a[i]==" ":
 		del a[i]
 		break
@@ -10,4 +11,3 @@ for i in range(len(a)-1,0,-1):
 		print("delete")
 		del a[i]
 print (a)
-
